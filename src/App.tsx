8c0311914ad8bb279a0308e40db3ed82734a9889
Keith Hamm <hkhamm@gmail.com>
2019-09-20 09:44:12 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react"
+import { makeStyles } from "@material-ui/core"
+import { useStore, useDispatch } from "./StoreProvider"
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles({
+    app: {
+        textAlign: "center"
+    },
+    appLogo: {
+        animation: "$appLogoSpin infinite 20s linear",
+        height: "40vmin",
+        pointerEvents: "none"
+    },
+    appHeader: {
+        backgroundColor: "#282c34",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "calc(10px + 2vmin)",
+        color: "white"
+    },
+    appLink: {
+        color: "#61dafb"
+    },
+    "@keyframes appLogoSpin": {
+        from: {
+            transform: "rotate(0deg)"
+        },
+        to: {
+            transform: "rotate(360deg)"
+        }
+    }
+})
+
+const App: FC = () => {
+    const classes = useStyles()
+    const { storeA, storeC, storeB } = useStore()
+    const dispatch = useDispatch()
+
+    const addOneToA = () => dispatch({ type: "a", value: storeA.a + 1 })
+    const addOneToB = () => dispatch({ type: "b", value: storeB.b + 1 })
+    const clear = () => {
+        dispatch({ type: "a", value: 0 })
+        dispatch({ type: "b", value: 0 })
+    }
+
+    return (
+        <div className={classes.app}>
+            <header className={classes.appHeader}>
+                <p>{`A + B = C: ${storeA.a} + ${storeB.b} = ${storeC.c}`}</p>
+                <button onClick={addOneToA}>Add 1 to A</button>
+                <button onClick={addOneToB}>Add 1 to B</button>
+                <button onClick={clear}>Clear</button>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/StoreProvider.tsx
@@ -0,0 +1,79 @@
+import React, { createContext, Dispatch, FC, useContext, useReducer } from "react"
+
+export interface Store {
+    storeA: {
+        a: number
+    }
+    storeB: {
+        b: number
+    }
+    storeC: {
+        c: number
+    }
+}
+
+export interface Action {
+    type: string
+    value: any
+}
+
+export const StoreContext = createContext<Store>({} as Store)
+
+export const useStore = () => useContext<Store>(StoreContext)
+
+export const DispatchContext = createContext<Dispatch<Action>>((action: Action) => {})
+
+export const useDispatch = () => useContext<Dispatch<Action>>(DispatchContext)
+
+function reducer(store: Store, action: Action) {
+    switch (action.type) {
+        case "a":
+            return {
+                ...store,
+                storeA: {
+                    ...store.storeA,
+                    a: action.value
+                },
+                storeC: {
+                    ...store.storeC,
+                    c: action.value + store.storeB.b
+                }
+            }
+        case "b":
+            return {
+                ...store,
+                storeB: {
+                    ...store.storeA,
+                    b: action.value
+                },
+                storeC: {
+                    ...store.storeC,
+                    c: store.storeA.a + action.value
+                }
+            }
+        default:
+            throw new Error()
+    }
+}
+
+const StoreProvider: FC = ({ children }) => {
+    const [store, dispatch] = useReducer(reducer, {
+        storeA: {
+            a: 0
+        },
+        storeB: {
+            b: 0
+        },
+        storeC: {
+            c: 0
+        }
+    })
+
+    return (
+        <DispatchContext.Provider value={dispatch}>
+            <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+        </DispatchContext.Provider>
+    )
+}
+
+export default StoreProvider
